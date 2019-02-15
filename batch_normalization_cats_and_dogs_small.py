dbcf43112fd189e12b343cb19ebbe473f086dc2f
qiuweimin1332499 <47662220+qiuweimin1332499@users.noreply.github.com>
2019-02-15 21:56:21 +0800

    Add files via upload

---
+++ batch_normalization_cats_and_dogs_small.py
@@ -0,0 +1,80 @@
+#使用batch-normalization及data-augmented阻滞过拟合
+from keras.preprocessing.image import ImageDataGenerator
+import os,shutil
+base_dir='C:\\Users\\lenovo\\.keras\\datasets\\cats_and_dogs_small'
+#保存较小数据集的目录
+train_dir=os.path.join(base_dir,'train')
+#训练集目录
+validation_dir=os.path.join(base_dir,'validation')
+#验证集目录
+test_dir=os.path.join(base_dir,'test')
+#测试集目录
+train_datagen=ImageDataGenerator(rescale=1./255,
+                                 rotation_range=40,
+                                 width_shift_range=0.2,
+                                 height_shift_range=0.2,
+                                 shear_range=0.2,
+                                 zoom_range=0.2,
+                                 horizontal_flip=True)
+test_datagen=ImageDataGenerator(rescale=1./255)
+#验证图像缩放与训练图像增强
+train_generator=train_datagen.flow_from_directory(train_dir,
+                                                  target_size=(150,150),
+                                                  batch_size=40,
+                                                  class_mode='binary')
+validation_generator=test_datagen.flow_from_directory(validation_dir,
+                                                      target_size=(150,150),
+                                                      batch_size=40,
+                                                      class_mode='binary')
+#图像调整大小与批量化生成
+
+
+from keras import layers
+from keras import models
+from keras import optimizers
+model=models.Sequential()
+model.add(layers.Conv2D(32,(3,3),activation='relu',input_shape=(150,150,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(64,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(128,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(128,(3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.BatchNormalization())
+model.add(layers.Flatten())
+model.add(layers.Dense(512,activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid'))
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+history=model.fit_generator(train_generator,
+                            steps_per_epoch=100,
+                            epochs=30,
+                            validation_data=validation_generator,
+                            validation_steps=50)
+model.save('cats_and_dogs_small_2.h5')
+#带有BN的神经网络
+
+
+import matplotlib.pyplot as plt
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(1,len(acc)+1)
+plt.plot(epochs,acc,'bo',label='training acc')
+plt.plot(epochs,val_acc,'b',label='validation acc')
+plt.title('training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs,loss,'bo',label='training loss')
+plt.plot(epochs,val_loss,'b',label='validation loss')
+plt.title('training and validation loss')
+plt.legend()
+plt.show()
